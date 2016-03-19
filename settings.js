1bd8b184efc00133ab564c76434940f1d2b332ba
Uncle CHEN <114413511@qq.com>
2016-03-19 19:07:04 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-        host: '117.121.25.131',
-        user: 'root',
-        password: 'password123',
-        database:'hxcms',
+        host: '',
+        user: '',
+        password: '',
+        database:'',
         port: 3306
 };
